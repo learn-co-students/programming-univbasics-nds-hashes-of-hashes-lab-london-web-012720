433fc95f60ed83bb177f106a7d319a45143344f9
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-14 14:43:16 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,10 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+  PORTION_1-PORTION_4 = {
+    :kingdom => PORTION_1,
+    :order = > PORTION_2,
+    :family => PORTION_3,
+    :species => PORTION_4
+  }
 end
